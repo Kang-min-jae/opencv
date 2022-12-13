5238497bc808304e06b9eb8b5391c20aa8c1d186
leejaeseok123 <112832262+leejaeseok123@users.noreply.github.com>
2022-12-13 12:06:23 +0900

    Update detect_face.py.py

--- opencvproject/detect_face.py.py
+++ opencvproject/detect_face.py.py
@@ -1,1 +1,24 @@
-opencvproject.py
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cat_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface.xml')
+
+img = cv2.imread('with2.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 3)
+cats = cat_cascade.detectMultiScale(gray, 1.09,1)
+
+for(x, y, w, h) in faces:
+    print(x,y,w,h)
+    cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0),2)
+
+for(x, y, w, h) in cats:
+    print(x,y,w,h)
+    cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0),2)
+
+print('사람 : ',str(len(faces)))
+print('고양이 : ',str(len(cats)))
+cv2.imshow('img', img)
+cv2.waitKey()
+#python detect_face.py
